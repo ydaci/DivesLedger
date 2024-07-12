d2648c905ca3c456ef7e6d4b1d12ecf14c9d5a08
ydaci <ydaci59@gmail.com>
2024-07-12 13:56:39 +0200

    MAJ Script de déploiement

--- backend/ignition/modules/DivesLedger.js
+++ backend/ignition/modules/DivesLedger.js
@@ -1,8 +1,11 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("DivesLedgerModule", (m) => {
-
-  const divesLedger = m.contract("DivesLedger");
-
-  return { divesLedger };
+  try {
+    const divesLedger = m.contract("DivesLedger");
+    return { divesLedger };
+  } catch (error) {
+    console.error("An error occurred while deploying the contract:", error);
+    throw error;
+  }
 });
